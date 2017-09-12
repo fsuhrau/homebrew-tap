7f490d2b1e95b1b2b2177e41ee31a4fe48f212be
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-12 19:50:51 +0200

    gocat version v0.1.1

--- Formula/gocat.rb
+++ Formula/gocat.rb
@@ -1,9 +1,9 @@
 class Gocat < Formula
   desc "gocat add coloring for logcat output"
   homepage "https://github.com/fsuhrau/gocat"
-  url "https://github.com/fsuhrau/gocat/releases/download/v0.1.0/gocat_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "0caf11efb1ed03a935222568684d91f1f114285fa7d7f75c7ad67a3e61b592b4"
+  url "https://github.com/fsuhrau/gocat/releases/download/v0.1.1/gocat_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "96f0226fdcf254091fcf125d41bdf0bd8a75719355e4601261b61e5ab3b7dfcd"
 
   def install
     bin.install "gocat"
